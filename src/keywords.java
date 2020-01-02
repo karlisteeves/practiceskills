b1bd14d716f90aa4b19c6cda806645adf87c8f36
Karli Steeves <steeves.karli@gmail.com>
2020-01-01 23:46:49 -0600

    working on expressions

---
+++ src/keywords.java
@@ -0,0 +1,12 @@
+public class keywords {
+    public static void main(String[] args) {
+        //a mile is equal to 1.609 km
+
+        double kilometers = (100 * 1.609);
+
+        int highScore = 50;
+        if(highScore == 50){
+            System.out.println("This is an expression!");
+        }
+    }
+}
