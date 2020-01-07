39e59fb24b6afeec98ea526794052b8e1577ef4d
Karli Steeves <steeves.karli@gmail.com>
2020-01-06 21:38:43 -0600

    redefining variables

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,28 @@ public class Main {
         int bonus = 100;
 
         if(score < 5000){
-            System.out.println("Your score was " + score);
-        } else {
-            System.out.println("Your score was " + score);
+            System.out.println("Your score is " + score);
+        } else if (score < 1000) {
+            System.out.println("Your score is " + score);
+        }else{
+            System.out.println("Your score is high!!");
         }
+
+
+
+        if (gameOver){ //you can put just gameOver instead of gameOver == true, it does the same thing.
+            int finalScore = score + (levelCompleted + bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+        //scope will deal with the accessibility of certain code in certain areas
+
+
+        //if you just take the int out, you can use the old variable names and it will just redefine them :0 There are disadvantages to this method, however.
+        int secondSore = 10000;
+        int newLevelCompleted = 8;
+        int newBonus = 200;
+        int finalScore = secondSore + (newLevelCompleted + newBonus);
+        System.out.println("Your NEW final score is " + finalScore);
+
     }
 }
