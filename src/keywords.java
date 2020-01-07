266a89b81692181839aff621fc34de1758de7d4a
Karli Steeves <steeves.karli@gmail.com>
2020-01-06 21:02:51 -0600

    working on statement drills

--- src/keywords.java
+++ src/keywords.java
@@ -3,10 +3,16 @@ public class keywords {
         //a mile is equal to 1.609 km
 
         double kilometers = (100 * 1.609);
+        System.out.println("these are km " + kilometers);
 
         int highScore = 50;
         if(highScore == 50){
             System.out.println("This is an expression!");
+
+            int myVariable = 50;
+            myVariable++;
+            myVariable--;
+            System.out.println("This is a test " );
         }
     }
 }
