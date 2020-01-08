cadb47cdcf0e3ce96066b067b9302528e8e4ddfa
Karli Steeves <steeves.karli@gmail.com>
2020-01-07 22:19:27 -0600

    working on cleaning methods

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 500;
-        int levelCompleted = 5;
-        int bonus = 100;
+
+        calculateScore(true, 500, 5,100);
+        calculateScore(true, 10000, 8, 200);
+
+        }
+
+
+
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
         if(score < 5000){
             System.out.println("Your score is " + score);
@@ -13,22 +19,7 @@ public class Main {
         }else{
             System.out.println("Your score is high!!");
         }
-
-
-
-        if (gameOver){ //you can put just gameOver instead of gameOver == true, it does the same thing.
-            int finalScore = score + (levelCompleted + bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
-        //scope will deal with the accessibility of certain code in certain areas
-
-
-        //if you just take the int out, you can use the old variable names and it will just redefine them :0 There are disadvantages to this method, however.
-        int secondSore = 10000;
-        int newLevelCompleted = 8;
-        int newBonus = 200;
-        int finalScore = secondSore + (newLevelCompleted + newBonus);
-        System.out.println("Your NEW final score is " + finalScore);
-
     }
+
+
 }
