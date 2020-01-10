99b5d7258b76e89321302f3cf442f7c5278c653a
Karli Steeves <steeves.karli@gmail.com>
2020-01-10 15:54:52 -0600

    workng on drills

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
 
-        calculateScore(true, 500, 5,100);
+        int highScore = calculateScore(true, 500, 5,100);
+        System.out.println("Final score will be " +highScore);
 
         calculateScore(true, 10000, 8, 200);
+
+        displayHighScorePosition("Randy", 5);
 
         }
 
@@ -18,8 +21,19 @@ public class Main {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        } else {
+            return -1;
         }
-        return finalScore;
+
+    }
+
+    public static int displayHighScorePosition(String playersName, int highScorePosition){
+
+
+        System.out.println(playersName + " managed to get into position " +highScorePosition + " on the table.");
+
+        return highScorePosition;
     }
 
 //void means don't send anything back, do not return anything
