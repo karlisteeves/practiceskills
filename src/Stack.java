435b6c7f0004307e04eb7511d83b6dca88252bbb
Karli Steeves <steeves.karli@gmail.com>
2020-03-24 14:26:44 -0500

    working with push

--- src/Stack.java
+++ src/Stack.java
@@ -2,5 +2,14 @@
 
 public class Stack{
 
+    int stack[] = new int[5];
+    int top = 0;
+    public void push(int data){
+
+        stack[top]= data;
+        top++;
+
+    }
+
 
 }
--- src/runner.java
+++ src/runner.java
@@ -4,5 +4,7 @@ public class runner{
     public static void main(String [] args){
 
         Stack items = new Stack();
+        items.push(5);
+        items.push(10);
     }
 }
