a59ce03202bcb42c161f28b292c7032557a4b1da
Karli Steeves <steeves.karli@gmail.com>
2020-03-24 14:36:57 -0500

    peek

--- src/Stack.java
+++ src/Stack.java
@@ -11,5 +11,26 @@ public class Stack{
 
     }
 
+    public int pop(){
+        int data;
+        top--;
+        data = stack[top];
+        stack[top] = 0;
+        return data;
+    }
+
+
+    public int peek(){
+        int data;
+        data = stack[top-1];
+        return data;
+    }
+
+    public void show(){
+        for(int n : stack){
+            System.out.print(n + " ");
+        }
+    }
+
 
 }
