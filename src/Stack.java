54ab9013c5db7b6893908455140ec1b79c79461b
Karli Steeves <steeves.karli@gmail.com>
2020-03-25 15:56:03 -0500

    stack drills

--- src/Stack.java
+++ src/Stack.java
@@ -3,38 +3,8 @@
 public class Stack{
 
     int stack[] = new int[5];
-    int top = 0;
-    public void push(int data){
 
-        stack[top]= data;
-        top++;
+    public void push(int data) {
 
     }
-
-    public int pop(){
-        int data;
-        top--;
-        data = stack[top];
-        stack[top] = 0;
-        return data;
-    }
-
-
-    public int peek(){
-        int data;
-        data = stack[top-1];
-        return data;
-    }
-
-    public int size(){
-
-    }
-
-    public void show(){
-        for(int n : stack){
-            System.out.print(n + " ");
-        }
-    }
-
-
 }
--- src/runner.java
+++ src/runner.java
@@ -5,12 +5,5 @@ public class runner{
 
         Stack items = new Stack();
         items.push(5);
-        items.push(8);
-        System.out.println(items.peek( ));
-        items.push(10);
-
-
-        System.out.println(items.pop());
-        items.show();
     }
 }
