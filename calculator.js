9607b799ca4bc44bbd5caa99014395d417e97404
Karli Steeves <steeves.karli@gmail.com>
2020-04-04 21:32:34 -0500

    making for each loops

--- calculator.js
+++ calculator.js
@@ -2,10 +2,13 @@ class Calculator{
      constructor(previousOperandTextElement, currentOperandTextElement){
          this.previousOperandTextElement = previousOperandTextElement
          this.currentOperandTextElement = currentOperandTextElement
+         this.clear()
      }
 
      clear(){
-
+    this.currentOperand = '';
+    this.previousOperand = '';
+    this.operation = undefined;
      }
 
      delete(){
@@ -37,3 +40,7 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator  = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach()
