a845de9d42131a67a82ac8917a3caf0359f15e97
Karli Steeves <steeves.karli@gmail.com>
2020-06-23 20:20:20 -0500

    js set up function

--- pracprac.js
+++ pracprac.js
@@ -1,1 +1,16 @@
-/////test
+
+window.onload = function(){
+
+    num = document.querySelectorAll(".num");
+    bar = document.querySelectorAll(".bar");
+    tc = document.querySelectorAll(".tc");
+    tl = document.querySelectorAll(".tl");
+    tr = document.querySelectorAll(".tr");
+    cc = document.querySelectorAll(".cc");
+    bl = document.querySelectorAll(".bl");
+    br = document.querySelectorAll(".br");
+    bc = document.querySelectorAll(".bc")
+    apMeri = document.querySelector(".apMeri")
+
+    setInterval(setTime, 1000);
+}
