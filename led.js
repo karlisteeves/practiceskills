9f28f9439b0424dd59feeffbe8fc6bed85eb77f0
Karli Steeves <steeves.karli@gmail.com>
2020-06-29 21:47:00 -0500

    switch case js

---
+++ led.js
@@ -0,0 +1,12 @@
+switch(tim.charAt(0)){
+    case "0": zero(0);break;
+    case "1":  one(0);break;
+    case "2":  two(0);break;
+    case "3":three(0);break;
+    case "4": four(0);break;
+    case "5": five(0);break;
+    case "6":  six(0);break;
+    case "7":seven(0);break;
+    case "8":eight(0);break;
+    case "9": nine(0);break;
+}
