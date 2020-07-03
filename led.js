68762efd53d003d39e3a3b98ce32cf465ff993cc
Karli Steeves <steeves.karli@gmail.com>
2020-07-03 18:45:30 -0500

    adding js

--- led.js
+++ led.js
@@ -10,3 +10,26 @@ switch(tim.charAt(0)){
     case "8":eight(0);break;
     case "9": nine(0);break;
 }
+switch(tim.charAt(1)){
+    case "0": zero(1);break;
+    case "1":  one(1);break;
+    case "2":  two(1);break;
+    case "3":three(1);break;
+    case "4": four(1);break;
+    case "5": five(1);break;
+    case "6":  six(1);break;
+    case "7":seven(1);break;
+    case "8":eight(1);break;
+    case "9": nine(1);break;
+}switch(tim.charAt(2)){
+    case "0": zero(2);break;
+    case "1":  one(2);break;
+    case "2":  two(2);break;
+    case "3":three(2);break;
+    case "4": four(2);break;
+    case "5": five(2);break;
+    case "6":  six(2);break;
+    case "7":seven(2);break;
+    case "8":eight(2);break;
+    case "9": nine(2);break;
+}
